56b02d750512c994efc4426f4e4b3f07d865b8b4
Raphael Jay <iteachmyselfwebdev@gmail.com>
2022-09-19 21:37:16 +0800

    console log fetched data

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-export const App = () => {
-  return <div>App</div>
+function App() {
+  const [coins, setCoins] = useState([])
+
+  const url =
+    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false'
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((res) => {
+        setCoins(res.data)
+        console.log(res.data[0])
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [])
+
+  return <></>
 }
+
+export default App
